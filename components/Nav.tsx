952d463cbe8662e1e5a73e24d261f19dd5958d0e
Hersh Thakur <hersh.thakur@gmail.com>
2023-11-19 18:54:31 +0000

    basic navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,16 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
 const Nav = () => {
+  const pathname = usePathname();
+
+
   return (
     <header>
-      <nav className=" w-screen flex items-center justify-between shadow-lg p-4">
+      <nav className=" flex w-screen items-center justify-between p-4 shadow-lg">
         <div className="rounded-lg bg-gradient-to-tr from-purple-700 to-emerald-400 shadow-md">
           <Link href="/">
             <h1 className="m-1 font-semibold text-slate-100">HT</h1>
           </Link>
         </div>
         <div className="flex gap-5">
-          <Link href={`/about`}>About</Link>
-          <Link href={`/contact`}>Contact</Link>
+          <Link
+            href={`/about`}
+            className={pathname === "/about" ? "text-purple-700" : "text-black"}
+          >
+            About
+          </Link>
+          <Link
+            href={`/contact`}
+            className={
+              pathname === "/contact" ? "text-purple-700" : "text-black"
+            }
+          >
+            Contact
+          </Link>
         </div>
       </nav>
     </header>
